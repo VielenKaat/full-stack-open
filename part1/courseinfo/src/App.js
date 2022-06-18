22f5843d62fc6102c9d7e9e9b81ef07aa5e96b51
vielenkaat <fattesttabcat@gmail.com>
2022-06-18 19:09:06 +0800

    part 1.4

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -6,6 +6,7 @@ const Header = (props) => {
 
 const Content = (props) => {
     let { parts } = props
+    console.log(parts)
     console.log(parts[0])
     return (
         <>
@@ -34,28 +35,30 @@ const Part = (props) => {
 }
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = {
-        name: 'Fundamentals of React',
-        exercises: 10
-    }
+    const parts =
+        [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
 
-    const part2 = {
-        name: 'Using props to pass data',
-        exercises: 7
-    }
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
 
-    const part3 = {
-        name: 'State of a component',
-        exercises: 14
-    }
-
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
     return (
         <div>
             <Header course={course} />
 
-            <Content parts={[part1, part2, part3]} />
+            <Content parts={parts} />
 
-            <Total parts={[part1, part2, part3]} />
+            <Total parts={parts} />
         </div>
     )
 
