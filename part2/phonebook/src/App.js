55c9421e2b416bc2a0b2b0b9002dc645849249f1
vielenkaat <fattesttabcat@gmail.com>
2022-07-01 21:49:27 +0800

    part 2.10

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,29 @@
 import { useState } from 'react'
 
+const Filter = ({ search, eventHandler }) => <div>filter shown with <input value={search} onChange={eventHandler} /></div>
+const PersonForm = ({ submitForm, newName, handleNameChange, newNumber, handlePhoneChange }) =>
+  <form onSubmit={submitForm}>
+    <div>
+      name: <input value={newName} onChange={handleNameChange} />
+    </div>
+    <div>
+      number: <input value={newNumber} onChange={handlePhoneChange} />
+    </div>
+    <div>
+      <button type="submit">add</button>
+    </div>
+  </form>
+
+const Persons = ({ shownItems }) =>
+  <ul>
+    {shownItems().map((person) =>
+
+      <li key={person.name}>
+        {person.name} {person.number}
+      </li>
+    )}
+  </ul>
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456', id: 1 },
@@ -54,28 +78,19 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>filter shown with <input value={search} onChange={handleSearchChange} /></div>
+      <Filter search={search} />
       <h2>Add new person</h2>
-      <form onSubmit={submitForm}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handlePhoneChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <ul>
-        {shownItems().map((person) =>
+      <PersonForm
+        submitForm={submitForm}
+        eventHandler={handleSearchChange}
+        newName={newName}
+        handlenameChange={handleNameChange}
+        newNumber={newNumber}
+        handlePhoneChange={handlePhoneChange} />
 
-          <li key={person.name}>
-            {person.name} {person.number}
-          </li>
-        )}
-      </ul>
+
+      <h2>Numbers</h2>
+      <Persons shownItems={shownItems} />
     </div>
   )
 }
